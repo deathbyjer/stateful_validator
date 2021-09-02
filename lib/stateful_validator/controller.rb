585f04c8caa9ce80a69738e22cb67d7cd5d8f23a
Jeremy Linder <jlinder@boardpackager.com>
2021-09-01 23:28:49 -0400

    refactor and allow classes

--- lib/stateful_validator/controller.rb
+++ lib/stateful_validator/controller.rb
@@ -1,82 +1,42 @@
 module StatefulValidator::Controller
   extend ActiveSupport::Concern
 
-  class << self
-    def sanitizer(instance, action)
-      instance.class.send(:_stateful_sanitizers)[action.to_sym] || instance.class.send(:_stateful_sanitizers)[nil]
-    end
-
-    def validator(instance, action)
-      instance.class.send(:_stateful_validators)[action.to_sym] || instance.class.send(:_stateful_validators)[nil]
-    end
-
-
-
-    # This will fill the validations that is an array of arrays, where the internal array has the
-    # following format:
-    #
-    # [:error_type, :validation, "error string"]
-    #
-    # So an example may be
-    #
-    # [:price_must_not_be_zero, "invalid price", :price]
-    def auto_fill_validations(instance, validations)
-      validations.each do |validation|
-        next unless validation.is_a?(Array)
-        next unless validation.count == 3
-
-        validator = instance.validator
-        error_key, error, the_validation = Array.wrap(validation[0]), validation[2], validation[1]
-        next unless the_validation.is_a?(Proc) || validator.respond_to?(the_validation) 
-
-        validated = if the_validation.is_a?(Proc) 
-          the_validation.call
-        else 
-          validator.send(the_validation)
-        end
-
-        error_key.each do |key| 
-          errors[key] = error unless errors[key] 
-        end unless validator
-      end
-    end
-  end
-
-
   module ClassMethods
     def sanitizer(klass = nil, options = {})
-      return _stateful_sanitizers[nil] if klass.nil?
-      return unless klass.ancestors.include?(ApplicationSanitizer)
+      return _sanitizers[nil] if klass.nil?
+      return unless klass.ancestors.include?(StatefulValidator::Sanitizer)
 
-      sanitizer = { 
-        klass: klass, 
-        ids: options[:ids],
-        param_key: options[:param_key] || klass.name.underscore.split("_")[0..-2].join("_").to_sym
-      }.compact
+      sanitizer = { klass: klass }
+      sanitizer[:param_key] = options[:param_key] || klass.name.underscore.split("_")[0..-2].join("_").to_sym
+      sanitizer[:ids] = options[:ids] if options[:ids]
       
-      Array.wrap(options[:only]).each do |action| 
-        _stateful_sanitizers[action&.to_sym] = sanitizer 
-      end
+      Array(options[:only] || [nil]).each {|action| _sanitizers[action ? action.to_sym : action] = sanitizer }
     end
 
     def validator(klass = nil, options = {})
-      return _stateful_validators[nil] if klass.nil?
-      return unless klass.ancestors.include?(ApplicationValidator)
+      return _validators[nil] if klass.nil?
+      return unless klass.ancestors.include?(StatefulValidator::Validator)
 
       validator = { klass: klass }
-      Array.wrap(options[:only]).each do |action| 
-        _stateful_validators[action&.to_sym] = validator 
-      end
+      Array(options[:only] || [nil]).each {|action| _validators[action ? action.to_sym : action] = validator }
+    end
+
+    def _lookup_sanitizer(action)
+      _sanitizers[action.to_sym] || _sanitizers[nil]
+    end
+
+    def _lookup_validator(action)
+      _validators[action.to_sym] || _validators[nil]
     end
 
     private
 
-    def _stateful_sanitizers
-      @_stateful_sanitizers ||= {}
+    def _sanitizers
+      @_sanitizers ||= {}
     end
 
-    def _stateful_validators
-      @_stateful_validators ||= {}
+    def _validators
+      @_validators ||= {}
     end
   end
 
@@ -87,12 +47,10 @@ module StatefulValidator::Controller
   def sanitizer
     return @sanitizer if @sanitizer
 
-    details = StatefulValidator::Controller.sanitizer self, params[:action]
+    details = self.class._lookup_sanitizer params[:action]
     return nil unless details
 
-    permitted = request.params[details[:param_key]]
-
-    @sanitizer = details[:klass].new permitted, controller: self, model: self.model
+    @sanitizer = details[:klass].new params[details[:param_key]], controller: self
     @sanitizer.set_ids_from_params! params, details[:ids]
     @sanitizer
   end
@@ -100,7 +58,7 @@ module StatefulValidator::Controller
   def validator
     return @validator if @validator
 
-    details = StatefulValidator::Controller.validator self, params[:action]
+    details = self.class._lookup_validator params[:action]
     return nil unless details
 
     @validator = details[:klass].new(controller: self, sanitizer: sanitizer)
@@ -118,7 +76,7 @@ module StatefulValidator::Controller
     validations = block.call(sanitizer)
     return unless validations.is_a?(Array)
 
-    StatefulValidator::Controller.auto_fill_validations self, validations 
+    auto_fill_validations validations 
   end
 
   def populate(&block)
@@ -127,7 +85,7 @@ module StatefulValidator::Controller
     ActiveRecord::Base.transaction do 
       begin
         block.call(sanitizer)
-      rescue ActiveModel::ValidationError => e
+      rescue ValidationError => e
         errors.merge! e.errors
       rescue ActiveRecord::RecordInvalid => e
         errors.merge! e.record.errors
@@ -138,5 +96,26 @@ module StatefulValidator::Controller
   end
 
   private
+
+  # This will fill the validations that is an array of arrays, where the internal array has the
+  # following format:
+  #
+  # [:error_type, :validation, "error string"]
+  #
+  # So an example may be
+  #
+  # [:price_must_not_be_zero, "invalid price", :price]
+  def auto_fill_validations(validations)
+    validations.each do |validation|
+      next unless validation.is_a?(Array)
+      next unless validation.count == 3
+      error_key, error, the_validation = validation[0], validation[2], validation[1]
+
+      next unless the_validation.is_a?(Proc) || validator.respond_to?(the_validation) 
+      error_key = [error_key] unless error_key.is_a?(Array)
+      validated = the_validation.is_a?(Proc) ? the_validation.call : validator.send(the_validation)
+      error_key.each {|key| errors[key] = error unless errors[key] } unless validated
+    end
+  end
 
 end
