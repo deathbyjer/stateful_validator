7595850e52f81764cc2c0ffac2b2ad5c6bc8dadd
Jeremy Linder <jlinder@boardpackager.com>
2021-11-30 09:31:43 -0500

    make sure to require the validation error

--- lib/stateful_validator.rb
+++ lib/stateful_validator.rb
@@ -4,6 +4,7 @@ module StatefulValidator
   require "stateful_validator/controller"
   require "stateful_validator/sanitizer"
   require "stateful_validator/validator"
+  require "stateful_validator/validation_error"
 end
 
 ActiveSupport.on_load(:action_controller) do
