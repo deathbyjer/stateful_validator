b09c089d506cb55b89d9db5ba1be18df26a53759
Jeremy Linder <jlinder@boardpackager.com>
2022-12-12 23:06:13 -0500

    use model from controller if one isn't provided to sanitizer

--- lib/stateful_validator/sanitizer.rb
+++ lib/stateful_validator/sanitizer.rb
@@ -10,7 +10,7 @@ class StatefulValidator::Sanitizer
   def initialize(params, options = {})
     self.params = params || {}
     self.controller = options[:controller]
-    self.model = options[:model]
+    self.model = options[:model] || self.controller&.model
   end
 
   def set_ids_from_params!(params, ids)
